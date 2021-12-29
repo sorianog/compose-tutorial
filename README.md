7ff9382e31936ba87185166e9d373f950ccac7a2
sorianog <13261711+sorianog@users.noreply.github.com>
2021-12-29 03:44:47 -0800

    Completed Lesson 2 of tutorial

--- README.md
+++ README.md
@@ -2,5 +2,14 @@
 ### Tutorial from: https://developer.android.com/jetpack/compose/tutorial  
 ---
 ## Lesson 1: Composable functions  
-- @Composable annotation
+- `@Composable` annotation
 - Define app’s UI programmatically
+- Composable functions can only be called from other composable functions
+
+## Lesson 2: Layouts
+- UI elements are organized in a hierarchy
+- Can have nested elements
+- `Column` - arranges elements vertically
+- `modifiers` - used to configure/decorate composable
+
+Lesson 3: Material Design
--- app/src/main/java/com/sorianog/composetutorial/MainActivity.kt
+++ app/src/main/java/com/sorianog/composetutorial/MainActivity.kt
@@ -3,11 +3,18 @@ package com.sorianog.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.sorianog.composetutorial.ui.theme.ComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,9 +24,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeTutorialTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Aloha 🤙🏽")
-                }
+//                Surface(color = MaterialTheme.colors.background) {
+//                    Greeting("Aloha 🤙🏽")
+//                }
+
+                MessageCard(Message("Android", "Jetpack Compose"))
             }
         }
     }
@@ -36,4 +45,40 @@ fun DefaultPreview() {
     ComposeTutorialTheme {
         Greeting("Android")
     }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    // Add padding around our message
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_background),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                // Set image size to 40 dp
+                .size(40.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape)
+        )
+
+        // Add a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(text = msg.author)
+            // Add a vertical space between the author and message texts
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = msg.body)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard(
+        msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
+    )
 }
